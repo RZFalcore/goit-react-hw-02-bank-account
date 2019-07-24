4195ad735ccb5083960c2d30597c06c2444afd1e
RZFalcore <box.rz.box@gmail.com>
2019-07-24 15:48:18 +0300

    Inital commit

---
+++ src/Component/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <p>Hello world!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
